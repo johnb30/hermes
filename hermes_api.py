f786991cd4fbab0e4cb971057e57141bd4ff1f95
John Beieler <john.b30@gmail.com>
2015-06-18 16:52:49 -0400

    Bro, do you even thread?

--- hermes_api.py
+++ hermes_api.py
@@ -6,17 +6,17 @@ import json
 import logging
 import requests
 import geolocation
-# import jsonrpclib
-from simplejson import loads
-from flask import Flask, jsonify, make_response
-from flask.ext.restful import Api, Resource, reqparse
-from flask.ext.httpauth import HTTPBasicAuth
-from flask.ext.restful.representations.json import output_json
+from threading import Thread
+from tornado.ioloop import IOLoop
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
+from flask.ext.httpauth import HTTPBasicAuth
+from flask import Flask, jsonify, make_response
+from flask.ext.restful import Api, Resource, reqparse
+from flask.ext.restful.representations.json import output_json
 
-output_json.func_globals['settings'] = {'ensure_ascii': False, 'encoding':'utf8'}
+output_json.func_globals['settings'] = {'ensure_ascii': False,
+                                        'encoding': 'utf8'}
 
 app = Flask(__name__)
 api = Api(app)
@@ -58,15 +58,57 @@ class HermesAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('content', type=unicode, location='json')
+        args = self.reqparse.parse_args()  # setup the request parameters
+        self.content = args['content']
+        self.result = {}
         super(HermesAPI, self).__init__()
 
     def post(self):
         app.logger.info('Started processing content')
-        args = self.reqparse.parse_args()  # setup the request parameters
-        mitie_payload = {'content': args['content']} #.encode('utf-8')}
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-        mitie_ip = os.environ['MITIE_PORT_5001_TCP_ADDR']  # get MITIE address
+        funcs = [self.call_cliff, self.call_mitie, self.call_mordecai]
+        threads = [Thread(target=f) for f in funcs]
+        [t.start() for t in threads]
+        [t.join() for t in threads]
+
+        try:
+            if not self.result['CLIFF']:
+                app.logger.info('No CLIFF info.')
+            else:
+                apply_topic = ('SYR' in self.result['CLIFF']['country_vec'] or
+                               'IRQ' in self.result['CLIFF']['country_vec'])
+                if apply_topic:
+                    topics_ip = os.environ['TOPICS_PORT_5002_TCP_ADDR']
+                    topics_url = 'http://{}:{}'.format(topics_ip, '5002')
+                    topics_payload = {'content': self.content}
+                    topics_r = requests.post(topics_url,
+                                             json=topics_payload).json()
+                    topics_r = json.loads(topics_r)
+                else:
+                    topics_r = {}
+        except Exception as e:
+            app.logger.error(e)
+            topics_r = {}
+
+        self.result['topic_model'] = topics_r
+
+
+# Bye for now CoreNLP
+#        stanford_ip = os.environ['STANFORD_PORT_5003_TCP_ADDR']
+#        server = jsonrpclib.Server('http://' + stanford_ip + ":5003")
+#        stanford_r = loads(server.parse(args['content']))
+        stanford_r = {}
+        self.result['stanford'] = stanford_r
+
+        app.logger.info('Finished processing content.')
+        return self.result, 201
+
+    def call_mitie(self):
+        result_key = 'MITIE'
+        mitie_payload = {'content': self.content}  # .encode('utf-8')}
+
+        # get MITIE address
+        mitie_ip = os.environ['MITIE_PORT_5001_TCP_ADDR']
         mitie_url = 'http://{}:{}'.format(mitie_ip, '5001')
         # hit MITIE containter
         try:
@@ -83,10 +125,14 @@ class HermesAPI(Resource):
             app.logger.error(e)
             mitie_r = {}
 
+        self.result[result_key] = mitie_r
+
+    def call_cliff(self):
+        result_key = 'CLIFF'
         cliff_ip = os.environ['CLIFF_PORT_8080_TCP_ADDR']
         cliff_url = 'http://{}:{}/CLIFF-2.0.0/parse/text'.format(cliff_ip,
                                                                  '8080')
-        cliff_payload = {'q': args['content']} #.encode('utf-8')}
+        cliff_payload = {'q': self.content}  # .encode('utf-8')}
         try:
             cliff_t = requests.get(cliff_url, params=cliff_payload)
         except requests.exceptions.RequestException as e:
@@ -102,12 +148,16 @@ class HermesAPI(Resource):
             app.logger.error(e)
             cliff_r = {}
 
+        self.result[result_key] = cliff_r
+
+    def call_mordecai(self):
+        result_key = 'mordecai'
         mordecai_ip = '52.5.183.171'
         mordecai_url = 'http://{}:{}/places'.format(mordecai_ip, '8999')
 
         try:
             mordecai_headers = {'Content-Type': 'application/json'}
-            mordecai_payload = json.dumps({'text': args['content']})
+            mordecai_payload = json.dumps({'text': self.content})
             mordecai_t = requests.post(mordecai_url, data=mordecai_payload,
                                        headers=mordecai_headers).json()
         except requests.exceptions.RequestException as e:
@@ -118,31 +168,8 @@ class HermesAPI(Resource):
         else:
             mordecai_r = mordecai_t
 
-        try:
-            if len(cliff_r) == 0:
-                app.logger.info('No CLIFF info.')
-            elif 'SYR' in cliff_r['country_vec'] or 'IRQ' in cliff_r['country_vec']:
-                topics_ip = os.environ['TOPICS_PORT_5002_TCP_ADDR']
-                topics_url = 'http://{}:{}'.format(topics_ip, '5002')
-                topics_payload = {'content': args['content']} #.encode('utf-8')}
-                topics_r = requests.post(topics_url, json=topics_payload).json()
-                topics_r = json.loads(topics_r)
-            else:
-                topics_r = {}
-        except Exception as e:
-            app.logger.error(e)
-            topics_r = {}
+        self.result[result_key] = mordecai_r
 
-
-# Bye for now CoreNLP
-#        stanford_ip = os.environ['STANFORD_PORT_5003_TCP_ADDR']
-#        server = jsonrpclib.Server('http://' + stanford_ip + ":5003")
-#        stanford_r = loads(server.parse(args['content']))
-        stanford_r = {}
-
-        app.logger.info('Finished processing content.')
-        return {'MITIE': mitie_r, 'CLIFF': cliff_r, 'topic_model': topics_r,
-                'stanford': stanford_r, 'mordecai': mordecai_r}, 201
 
 api.add_resource(HermesAPI, '/')
 
